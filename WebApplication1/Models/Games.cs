cc16cedb0d3543d35936b94233279eb20d0cab36
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-10-24 21:40:39 -0400

    Wrote code first data EF data model

---
+++ WebApplication1/Models/Games.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Game.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string GameName { get; set; }
+        public DateTime GameStartDate { get; set; }
+    }
+
+    public class Player
+    {
+        public int Id { get; set; }
+        public string PlayerFirstName { get; set; }
+        public string PlayerGameName { get; set; }
+    }
+
+    public class Score
+    {
+        public int Id { get; set; }
+        public int GameID { get; set; }
+        public int TurnNum { get; set; }
+        public DateTime TurnTime { get; set; }
+        public int PlayerID { get; set; }
+        public int TurnScore { get; set; }
+    }
+
+
+public class GameContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Score> Scores { get; set; }
+    }
+}
