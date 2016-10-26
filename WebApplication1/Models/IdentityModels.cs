4455ddd52e2b8f2552e6639a1839397d8cbc417e
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-10-26 11:59:02 -0400

    pre-scaffold safety commit

--- WebApplication1/Models/IdentityModels.cs
+++ WebApplication1/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
 namespace WebApplication1.Models
 {
@@ -29,6 +30,34 @@ namespace WebApplication1.Models
         {
             return new ApplicationDbContext();
         }
+        public class Game
+        {
+            public int Id { get; set; }
+            public string GameName { get; set; }
+            public System.DateTime GameStartDate { get; set; }
+            public virtual ApplicationUser UserId { get; set; }
+        }
+
+        public class Player
+        {
+            public int Id { get; set; }
+            public string PlayerFirstName { get; set; }
+            public string PlayerGameName { get; set; }
+        }
+
+        public class Score
+        {
+            public int Id { get; set; }
+            public int GameID { get; set; }
+            public int TurnNum { get; set; }
+            public DateTime TurnTime { get; set; }
+            public int PlayerID { get; set; }
+            public int TurnScore { get; set; }
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Score> Scores { get; set; }
     }
 
 }
